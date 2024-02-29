eb21e8fa29eface79cf3c1e0566db05b28ce3a78
unknown <anhkhoacttv@gmail.com>
2024-02-29 22:59:02 +0700

    Prop 1

--- src/components/prop/Children.vue
+++ src/components/prop/Children.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>Children work</div>
+  <p>Children work</p>
+  <p>My name is {{ `${firstName} ${lastName}` }}</p>
+  <p></p>
+  <button @click="updateNameInChildren(Nguyen)">
+    Update lastName to "Khoa"
+  </button>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  firstName: String,
+  lastName: {
+    type: String,
+    default: 'anonymous'
+    // required, validator, ...
+  }
+})
+
+const emit = defineEmits(['updateName'])
+
+const updateNameInChildren = (updateNameInChildren) => {
+  emit('updateName', 'Khoa')
+}
+</script>
--- src/components/prop/Parent.vue
+++ src/components/prop/Parent.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>Parent work <Children></Children></div>
+  <p>Parent work</p>
+  <input type="text" v-model="lastName" />
+  <br /><br />
+  <Children
+    firstName="Tran"
+    :last-name="lastName"
+    @updateName="updateNameInParent"
+  ></Children>
 </template>
 
 <script setup>
 import Children from './Children.vue'
+import { ref } from 'vue'
+
+const lastName = ref('')
+
+const updateNameInParent = (lastNameUpdate) => {
+  lastName.value = lastNameUpdate
+}
 </script>
